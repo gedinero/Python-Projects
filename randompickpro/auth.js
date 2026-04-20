2479ea9b2155ce001d2841f82c2f18fc6c028ea6
Ge Dinero <jgordon728@gmail.com>
2026-04-20 15:59:20 -0400

    progress

---
+++ randompickpro/auth.js
@@ -0,0 +1,44 @@
+const loginBtn = document.querySelector(".primary-btn");
+const createBtn = document.querySelector(".secondary-btn");
+
+const inputs = document.querySelectorAll("input");
+const usernameInput = inputs[0];
+const passwordInput = inputs[1];
+
+// CREATE ACCOUNT
+createBtn.addEventListener("click", () => {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (username === "" || password === "") {
+        alert("Fill in all fields");
+        return;
+    }
+
+    localStorage.setItem(username, password);
+
+    alert("Account created! You can now login.");
+
+    usernameInput.value = "";
+    passwordInput.value = "";
+});
+
+// LOGIN
+loginBtn.addEventListener("click", () => {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    const storedPassword = localStorage.getItem(username);
+
+    if (storedPassword === null) {
+        alert("User not found");
+        return;
+    }
+
+    if (storedPassword === password) {
+        alert("Login successful!");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Incorrect password");
+    }
+});
